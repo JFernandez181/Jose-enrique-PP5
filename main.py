af996c8017b75808e057058760f86345e2b380a0
Jose Enrique Enrique <114123898+JFernandez181@users.noreply.github.com>
2022-12-09 01:12:41 +0000

    Paso #4

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-with open('gift_costs.txt') as f:
-    gift_costs = f.read().split('\n')
+"""
+Jose Enrique Fernandez
+22-0330
+Refinando Código
+En este programa refinariamos el codigo para mejor calidad.
+"""
+
+
+def precios():
+    arc = open('gift_costs.txt', 'r')
+    precios = list(arc)
+    precios = [int(c) for c in precios]  
+    arc.close()  
+    return precios
+
+
+def total(precios):
+    total_price = 0
+    for cost in precios:
+        if cost > 1000:
+            total_price += cost * 1.16  
+        else:
+            total_price += cost  
+
+    return total_price
+
+
+def main():
+    print(total(precios()))
     
-gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
 
-total_price = 0
-for cost in gift_costs:
-    if cost > 1000:
-        total_price += cost * 1.16  # agrega impuestos
-    else:
-        total_price += cost
-
-print(total_price)
+if __name__ == '_main_':
+    main()
